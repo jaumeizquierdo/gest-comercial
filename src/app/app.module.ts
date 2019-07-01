6dbbfdc3028713b6e350cece035e0fa661482001
Jaume Izquierdo <jaumeizquierdo@gmail.com>
2019-07-01 14:02:18 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonsModule } from './commons/commons.module';
+import { CustomerModule } from './customer/customer.module';
+/* import { StoringModule } from './storing/storing.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { BillingModule } from './billing/billing.module'; */
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonsModule,
+    CustomerModule
+
+    // CustomerModule,
+    // StoringModule,
+    // SuppliersModule,
+    // BillingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule { }
---
+++ src/app/storing/storing-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { StoringlistComponent } from './storinglist/storinglist.component';
+import { StoringincomingComponent } from './storingincoming/storingincoming.component';
+import { StoringoutgoingComponent } from './storingoutgoing/storingoutgoing.component';
+
+const routes: Routes = [
+  {path: '', component: StoringlistComponent},
+  {path: 'in', component: StoringincomingComponent},
+  {path: 'out', component: StoringoutgoingComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class StoringRoutingModule { }
